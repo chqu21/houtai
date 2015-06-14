81cace8740250316e4e7d228d85bc3c737a86fbb
chqu21 <512888683@qq.com>
2015-06-14 11:21:35 +0800

    添加订单列表

--- App/Admin/Controller/OrderController.class.php
+++ App/Admin/Controller/OrderController.class.php
@@ -7,146 +7,107 @@ use Admin\Controller\CommonController;
  * @author wangdong
  */
 class OrderController extends CommonController {
-    /**
-	 * 后台首页
-	 */
-	public function index(){
-	    $admin_db      = D('Admin');
-	    $menu_db       = D('Menu');
-	    
-	    $userid = session('userid');
-		$userInfo = $admin_db->getUserInfo($userid);    //获取用户基本信息
-		$menuList = $menu_db->getMenu();                //头部菜单列表
-		
-		$this->assign('userInfo', $userInfo);
-		$this->assign('menuList', $menuList);
-		$this->display('index');
-	}
 
-    public function abc(){
-        $this->display('index');
-    }
-	
-	/**
-	 * 用户登录
-	 */
-    public function login(){
-    	if (I('get.dosubmit')){
-            $admin_db = D('Admin');
-            
-			$username = I('post.username', '', 'trim') ? I('post.username', '', 'trim') : $this->error('用户名不能为空', HTTP_REFERER);
-			$password = I('post.password', '', 'trim') ? I('post.password', '', 'trim') : $this->error('密码不能为空', HTTP_REFERER);
-			//验证码判断
-			$code = I('post.code', '', 'trim') ? I('post.code', '', 'trim') : $this->error('请输入验证码', HTTP_REFERER);
-			if(!check_verify($code, 'admin')) $this->error('验证码错误！', HTTP_REFERER);
-			
-			if($admin_db->login($username, $password)){
-			    $this->success('登录成功', U('Index/index'));
-			}else{
-			    $this->error($admin_db->error, HTTP_REFERER);
-			}
-    	}else {
-    		$this->display();
-    	}
-    }
-    
-    /**
-	 * 退出登录
-	 */
-    public function logout() {
-		session('userid', null);
-		session('roleid', null);
-		cookie('username', null);
-		cookie('userid', null);
-		
-		$this->success('安全退出！', U('Index/login'));
-	}
-    
-    /**
-	 * 验证码
-	 */
-	public function code(){
-        $verify = new \Think\Verify();
-        $verify->useCurve = true;
-        $verify->useNoise = false;
-        $verify->bg = array(255, 255, 255);
-        
-		if (I('get.code_len')) $verify->length = intval(I('get.code_len'));
-		if ($verify->length > 8 || $verify->length < 2) $verify->length = 4;
-		
-		if (I('get.font_size')) $verify->fontSize = intval(I('get.font_size'));
-		
-		if (I('get.width')) $verify->imageW = intval(I('get.width'));
-		if ($verify->imageW <= 0) $verify->imageW = 130;
-		
-		if (I('get.height')) $verify->imageH = intval(I('get.height'));
-		if ($verify->imageH <= 0) $verify->imageH = 50;
+    public $orderStatus = array(
+        '1' => '取消',
+        '2' => '待确认课酬',
+        '3' => '已确认课酬',
+        '4' => '待评价',
+        '5' => '已评价',
+        '6' => '未付款'
+    );
+    public $compareColumns = array(
+        'teacher_name','raw_add_time'
+    );
 
-        $verify->entry('admin');
-	}
-	
     /**
-     * 左侧菜单
+     * 老师列表
      */
-	public function public_menuLeft($menuid = 0) {
-	    $menu_db = D('Menu');
-		$datas = array();
-		$list = $menu_db->getMenu($menuid);
-		foreach ($list as $k=>$v){
-			$datas[$k]['name'] = $v['name'];
-			$son_datas = $menu_db->getMenu($v['id']);
-			foreach ($son_datas as $k2=>$v2){
-				$datas[$k]['son'][$k2]['text'] = $v2['name'];
-				$datas[$k]['son'][$k2]['id']   = $v2['id'];
-				$datas[$k]['son'][$k2]['url'] = U($v2['c'].'/'.$v2['a'].'?menuid='.$v2['id'].'&'.$v2['data']);
-			}
-		}
-		$this->ajaxReturn($datas);
-	}
-	
-	/**
-	 * 后台欢迎页
-	 */
-	public function public_main(){
-	    $admin_db      = D('Admin');
-	    $userid = session('userid');
-		$userInfo = $admin_db->getUserInfo($userid);    //获取用户基本信息
-		
-	    $sysinfo = \Admin\Common\Sysinfo::getinfo();
-		$os = explode(' ', php_uname());
-		//网络使用状况
-		$net_state = null;
-		if ($sysinfo['sysReShow'] == 'show' && false !== ($strs = @file("/proc/net/dev"))){
-			for ($i = 2; $i < count($strs); $i++ ){
-				preg_match_all( "/([^\s]+):[\s]{0,}(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/", $strs[$i], $info );
-				$net_state.="{$info[1][0]} : 已接收 : <font color=\"#CC0000\"><span id=\"NetInput{$i}\">".$sysinfo['NetInput'.$i]."</span></font> GB &nbsp;&nbsp;&nbsp;&nbsp;已发送 : <font color=\"#CC0000\"><span id=\"NetOut{$i}\">".$sysinfo['NetOut'.$i]."</span></font> GB <br />";
-			}
-		}
-		
-		$this->assign('userInfo', $userInfo);
-		$this->assign('sysinfo',$sysinfo);
-		$this->assign('os',$os);
-		$this->assign('net_state',$net_state);
-		$this->display('main');
-	}
-	
-	/**
-	 * 更新后台缓存
-	 */
-	public function public_clearCatche(){
-	    $list = dict('', 'Cache');
-		if(is_array($list) && !empty($list)){
-			foreach ($list as $modelName=>$funcName){
-				D($modelName)->$funcName();
-			}
-		}
-		$this->success('操作成功');
-	}
-	
-    /**
-     * 防止登录超时
-     */
-	public function public_sessionLife(){
-		$userid = session('userid');
-	}
+    public function orderList($page = 1, $rows = 10,$search = array(),$sort = 'order_id', $order = 'desc'){
+        if(IS_POST){
+            $master_db = D('OrderMaster');
+            $detail_db = D('OrderDetail');
+            $member_info_db = D('MemberInfo');
+            //搜索
+            $where = array();
+            if (!empty($search['payment_status']) && $search['payment_status']==2){
+                $search['payment_status'] = 0;
+            }
+            $newMember = 0;
+            $mIds = array();
+            if (!empty($search['new_member'])){
+                $newMember = 1;
+                unset($search['new_member']);
+                if (!empty($search['raw_add_time'])){
+                    $member_info = $member_info_db->where("`raw_add_time` like '%".$search['raw_add_time']."%'")->select();
+                    foreach($member_info as $m){
+                        $mIds[] = $m['member_id'];
+                    }
+                }
+            }
+
+            foreach ($search as $k=>$v){
+                if(!$v) continue;
+                if (in_array($k,$this->compareColumns)){
+                    $where[] = "`{$k}` like '%{$v}%'";
+                }else{
+                    $where[] = "`{$k}` = '{$v}'";
+                }
+
+            }
+            $where = implode(' and ', $where);
+            if (!empty($mIds)){
+                $where = $where.' and member_id in ('.implode(',',$mIds).')';
+            }
+            $limit=($page - 1) * $rows . "," . $rows;
+            $total = $master_db->where($where)->count();
+            $order = $sort.' '.$order;
+            $column = "`order_code`,`total_price`,`payment_price`,`total_time`,`payment_status`,`order_status`,`coupon_id`,`coupon_price`,`member_id`,`member_name`,`payment_method`,`teacher_id`,`teacher_name`,`order_id` as order_ids,`cancel_reason`,`raw_add_time`";
+            $list = $total ? $master_db->field($column)->where($where)->order($order)->limit($limit)->select() : array();
+            foreach($list as $k => $lt){
+                $orderCode = $lt['order_code'];
+                $mInfo =$detail_db->field("`class_method`,`class_address`,`student_name`,`student_mobile`,`course_name`,`course_price`,`course_time`,`class_begin_time`,`time_quantum`,`teacher_area`")->where("order_code=$orderCode")->select();
+                $list[$k] = array_merge($lt,$mInfo[0]);
+                if ($lt['payment_status']==1){
+                    $list[$k]['payment_status'] = '已付款';
+                }else{
+                    $list[$k]['payment_status'] = '未付款';
+                }
+                $list[$k]['order_status'] = $this->orderStatus[$lt['order_status']];
+            }
+            $data = array('total'=>$total, 'rows'=>$list);
+            $this->ajaxReturn($data);
+        }else{
+            $menu_db = D('Menu');
+            $currentpos = $menu_db->currentPos(I('get.menuid'));  //栏目位置
+            $datagrid = array(
+                'options'     => array(
+                    'title'   => $currentpos,
+                    'url'     => U('Order/orderList', array('grid'=>'datagrid')),
+                    'toolbar' => 'admin_orderlist_datagrid_toolbar',
+                ),
+                'fields' => array(
+                    '订单号'    => array('field'=>'order_code','width'=>15,'sortable'=>true),
+                    '课程名'      => array('field'=>'course_name','width'=>15,'sortable'=>true),
+                    '上课老师'      => array('field'=>'teacher_name','width'=>7,'sortable'=>true),
+                    '学生名'    => array('field'=>'student_name','width'=>7,'sortable'=>true),
+                    '学生手机号'  => array('field'=>'student_mobile','width'=>15,'sortable'=>true),
+                    '课时'    => array('field'=>'course_time','width'=>15,'sortable'=>true),
+                    '开始时间'    => array('field'=>'class_begin_time','width'=>15,'sortable'=>true),
+                    '时间段'    => array('field'=>'time_quantum','width'=>15,'sortable'=>true),
+                    '付款状态' => array('field'=>'payment_status','width'=>15,'sortable'=>true),
+                    '订单状态' => array('field'=>'order_status','width'=>15,'sortable'=>true),
+                    '优惠券ID' => array('field'=>'coupon_id','width'=>15,'sortable'=>true),
+                    '优惠券价格' => array('field'=>'coupon_price','width'=>25,'sortable'=>true),
+                    '下单时间' => array('field'=>'raw_add_time','width'=>25,'sortable'=>true),
+                    '订单总价' => array('field'=>'total_price','width'=>15,'sortable'=>true),
+                    '付款总价' => array('field'=>'total_price','width'=>15,'sortable'=>true),
+                    '操作'    => array('field'=>'order_id','width'=>55,'sortable'=>true,'formatter'=>'adminMemberListOperateFormatter'),
+                )
+            );
+            $this->assign('datagrid', $datagrid);
+            $this->display('order_list');
+        }
+    }
+
 }
